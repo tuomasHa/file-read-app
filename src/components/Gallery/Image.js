7fe594d9a136677c2ff47902c7e907b185c1b946
Tuomas Hakala <tuomas.hakala@eng.tamk.fi>
2016-11-14 15:16:31 +0200

    Prototype responsive grid layout for Gallery

--- src/components/Gallery/Image.js
+++ src/components/Gallery/Image.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default ({index, image}) =>{
-  return <img className='gallery-image' src={image.path} alt={image.name} />;
+  return <div className='gallery-image-container'>
+    <img className='gallery-image' src={image.path} alt={image.name} />
+  </div>;
 }
