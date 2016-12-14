db142c782286eedafb8f5639edae58cc02f9b19b
Tuomas Hakala <tuomas.hakala@eng.tamk.fi>
2016-12-14 12:13:17 +0200

    fix for scss bundling

--- front-end/src/entry.js
+++ front-end/src/entry.js
@@ -4,7 +4,7 @@ import Promise from 'es6-promise';
 Promise.polyfill(); //Promise API polyfill for 1e9+
 import 'whatwg-fetch'; //Fetch API polyfill for ie 10+
 import Routing from './components/Routing';
-require('./styles.scss'); //contains variables
+require('./root.scss');
 
 const root = document.body.appendChild(document.createElement('div'));
 
