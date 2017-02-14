015233984e2bd8f7b7629585a8481c417258b8e6
Tuomas Hakala <tuomas.hakala@eng.tamk.fi>
2017-02-14 13:20:47 +0200

    Use subsection title in gallery and add some general margin variables for sass

--- front-end/src/components/Gallery/ImageSubset.js
+++ front-end/src/components/Gallery/ImageSubset.js
@@ -8,13 +8,35 @@ const renderImage = (e, index, subIndex, handler) =>{
 export default class ImageSubset extends React.Component{
   constructor(props){
     super(props);
+
+    this.state = {
+      title: {},
+      images: props.folder.children
+    };
+    props.folder.children.forEach((e, i, a) => {
+      if (e.name === 'title.json') {
+        this.state.title = e;
+        this.state.images.splice(i, 1);
+      }
+    });
+    let title = this.state.title;
+    if (title && title.path) {
+      fetch(title.path).then((response) => {
+          return response.json();
+        }).then((arr) => {
+          if (arr && arr.length) {
+            title.text = arr[0];
+            this.setState({title});
+          }
+        });
+    }
   }
 
   render(){
     return <div className='gallery-image-subset'>
-      <span className='image-subset-header'>{'subset'}</span>
+      <span className='image-subset-header'>{this.state.title.text || ''}</span>
       <div className='image-subset-body'>
-        {this.props.folder.children.map((e, i) => renderImage(e, this.props.index, i, this.props.selectImage))}
+        {this.state.images.map((e, i) => renderImage(e, this.props.index, i, this.props.selectImage))}
       </div>
     </div>;
   }
